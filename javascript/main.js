6bb5e2af1081c3d58a290726d06025e0e6402359
JunguLiu <33013871+JunguLiu@users.noreply.github.com>
2020-08-29 11:28:57 -0600

    update

--- javascript/main.js
+++ javascript/main.js
@@ -58,3 +58,17 @@ function addTask() {
   div.appendChild(form);
   form.appendChild(input);
 }
+
+function ShowElement(element) {
+  var oldhtml = element.innerHTML;
+  var newobj = document.createElement("input");
+  newobj.type = "text";
+  newobj.value = oldhtml;
+  newobj.onblur = function () {
+    element.innerHTML = this.value == oldhtml ? oldhtml : this.value;
+  };
+  element.innerHTML = "";
+  element.appendChild(newobj);
+  newobj.setSelectionRange(0, oldhtml.length);
+  newobj.focus();
+}
