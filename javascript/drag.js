cfa4a4d6e6c27348e4b276d2aeca88ab828ecdce
sanasdh <sanasdh@gmail.com>
2020-08-29 12:30:00 -0400

    drag tasks

--- javascript/drag.js
+++ javascript/drag.js
@@ -1,15 +1,82 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
-let clmn1 = document.querySelector(".clmn1");
-
+let board1 = document.getElementById("board1");
+let board2 = document.getElementById("board2");
 /*----- event listeners -----*/
-clmn1.addEventListener("ondragstart", clickingTasks);
+// board1.addEventListener("ondragstart", clickingTasks);
+// board2.addEventListener("ondrop", drop);
+// board2.addEventListener("ondragover", allowDrop);
+
 /*----- functions -----*/
-function clickingTasks(e) {
-  clickedTask = e.target;
-  if (!clickedTask.classList.contains("tasks")) {
-    return;
-  }
-  e.dataTransfer.setData("text", e.target.id);
-}
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {}, false);
+
+document.addEventListener(
+  "dragstart",
+  function (event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = 0.5;
+  },
+  false
+);
+
+document.addEventListener(
+  "dragend",
+  function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+  },
+  false
+);
+
+/* events fired on the drop targets */
+document.addEventListener(
+  "dragover",
+  function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  },
+  false
+);
+
+document.addEventListener(
+  "dragenter",
+  function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "purple";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "dragleave",
+  function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "drop",
+  function (event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+    }
+  },
+  false
+);
