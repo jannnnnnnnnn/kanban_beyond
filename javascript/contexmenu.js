877a9cf4f2bc45a3fdaf1bf4571bcf25096de3da
sanasdh <sanasdh@gmail.com>
2020-08-30 18:20:14 -0400

    delete

--- javascript/contexmenu.js
+++ javascript/contexmenu.js
@@ -1,24 +1,20 @@
 var contextMenu = CtxMenu(".taskDiv");
 contextMenu.addItem("Move", moveBoard);
+contextMenu.addItem("Delete", deleteFunc);
+
 function moveBoard(e) {
   a = prompt("write the name of the column you want this column be next to:");
   a = a.toUpperCase();
-  console.log(e);
-  console.log(help);
   help.forEach(function (h) {
-    console.log(h);
-    console.log("here");
-    console.log(h.innerText.toUpperCase());
     let boardTitleName = h.innerText.toUpperCase();
     console.log(a == boardTitleName);
     if (a == boardTitleName) {
-      console.log("true");
       let parent = h.parentNode;
-      console.log(parent.parentNode.parentNode.appendChild(e));
       parent.parentNode.appendChild(e);
     }
   });
+}
 
-  console.log(a);
-  console.log(typeof a);
+function deleteFunc(e) {
+  e.remove();
 }
