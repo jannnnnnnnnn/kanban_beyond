2b8997d4077d19b546a382f0893dfefe2526a15f
sanasdh <sanasdh@gmail.com>
2020-08-31 10:32:38 -0400

    a

--- javascript/contexmenu.js
+++ javascript/contexmenu.js
@@ -1,22 +1,25 @@
 var contextMenu = CtxMenu(".taskDiv");
 contextMenu.addItem("Move", moveBoard);
 contextMenu.addItem("Delete", deleteFunc);
-
 function moveBoard(e) {
+  let count = 0;
+
   a = prompt("write the name of the column you want this task be in it:");
   a = a.toUpperCase();
   help.forEach(function (h) {
     let boardTitleName = h.innerText.toUpperCase();
     console.log(a == boardTitleName);
     if (a == boardTitleName) {
+      count++;
       let parent = h.parentNode;
       parent.parentNode.appendChild(e);
-    } else {
-      alert(
-        "Make sure you are writing the correct name, if the name includes :/;,! ... make sure to write them as well"
-      );
     }
   });
+  if (count == 0) {
+    alert(
+      "Make sure you are writing the correct name, if the name includes :/;,! ... make sure to write them as well"
+    );
+  }
 }
 
 function deleteFunc(e) {
